f0cfca4b0a319a6d7bb8eb19bb9f136e169258d1
Keno Budde <kenobudde@yahoo.de>
2016-12-02 15:58:18 +0000

    changes

--- Ex3.java
+++ Ex3.java
@@ -17,25 +17,24 @@ public class Ex3 {
 		public void printJunction(){
 			int dir = junctions.get(junctions.size() - 1);
 			int index = junctions.indexOf(dir);
-			String str = "";
-			switch(dir){
-				case IRobot.NORTH:
-					str = "NORTH";
-					break;
-				case IRobot.SOUTH:
-					str = "SOUTH";
-					break;
-				case IRobot.WEST:
-					str = "WEST";
-					break;
-				case IRobot.EAST:
-					str= "EAST";
-					break;
-			}
+			String str = direction(dir);
 			System.out.println("Junction " + index + " heading " + str);
 		}
 
 	}
+	private String direction(int dir){
+        switch(dir){
+            case IRobot.NORTH:
+                return "NORTH";
+            case IRobot.SOUTH:
+                return "SOUTH";
+            case IRobot.WEST:
+                return "WEST";
+            case IRobot.EAST:
+                return "EAST";
+        }
+        return "";
+    }
 	private RobotData robotData;
 	public void controlRobot(IRobot robot) {
 		if((robot.getRuns() == 0) && (pollRun == 0)){
@@ -88,6 +87,7 @@ public class Ex3 {
 		int heading = robot.getHeading();
 		ArrayList<Integer> passage = passageExits(robot);
 		int passageSize = passage.size();
+        System.out.println("Explore Mode: "+ explorerMode + " | case:" + passageSize + " | heading: " + heading);
 		if (passageSize == 3){
 			neverBefore(robot, heading);
 		}
@@ -95,17 +95,19 @@ public class Ex3 {
 			explorerMode=1;
 			return passage.get(randomizer(passageSize));
 		} else{
-			if(explorerMode==1){
-				return exits.get(randomizer(4));
-			} else {
-				int dir = IRobot.NORTH+(((robotData.junctions.get(robotData.junctions.size()-1)+2)%4+4)%4);
+            if(explorerMode==1){
+                explorerMode=0;
+                return IRobot.NORTH+((((heading-IRobot.NORTH)+2)%4+4)%4);
+            }
+                int dir2 = robotData.junctions.get(robotData.junctions.size()-1);
+			 	int dir = IRobot.NORTH+((dir2+2)%4+4)%4;
 				robotData.junctions.remove(robotData.junctions.size()-1);
-				neverBefore(robot,heading);
+                System.out.println("pulled of " + direction(dir2)+" | new direction:" + direction(dir));
+				//neverBefore(robot,heading);
 				return dir;			
 			}
 		}
 
-	}
 	private void neverBefore(IRobot robot, int heading){
 		robotData.add(heading);
 		junctionCounter++;
@@ -114,6 +116,7 @@ public class Ex3 {
 		int heading = robot.getHeading();
 		ArrayList<Integer> passage = passageExits(robot);
 		int passageSize = passage.size();
+        System.out.println("Explore Mode: "+ explorerMode + " | case:" + passageSize + " | heading: " + heading);
 		switch(passageSize){
 			case 2:
 				neverBefore(robot, heading);
@@ -121,20 +124,22 @@ public class Ex3 {
 				explorerMode=1;
 				return passage.get(randomizer(passageSize));	
 			default: 
-				if(explorerMode==1){
-					return exits.get(randomizer(3));
-				} else {
-
-					int dir = IRobot.NORTH+(((robotData.junctions.get(robotData.junctions.size()-1)+2)%4+4)%4);
-					robotData.junctions.remove(robotData.junctions.size()-1);
-					neverBefore(robot,heading);
+					if(explorerMode==1){
+						explorerMode=0;
+                        return IRobot.NORTH+((((heading-IRobot.NORTH)+2)%4+4)%4);
+					}
+                    int dir2 = robotData.junctions.get(robotData.junctions.size()-1);
+                    int dir = IRobot.NORTH+((dir2+2)%4+4)%4;
+                    System.out.println("pulled of " + direction(dir2)+" | new direction:" + direction(dir));
+                    robotData.junctions.remove(robotData.junctions.size()-1);
+					//neverBefore(robot,heading);
 					return dir;			
 
 				}
 
 		}
 
-	}
+
 	private int randomizer (int n){
 		return (int) (Math.random()*n);
 
@@ -143,7 +148,6 @@ public class Ex3 {
 		int coming = IRobot.NORTH + (((robot.getHeading()-IRobot.NORTH)+2)%4+4)%4;
 		int index = exits.indexOf(coming);
 		if(index!=-1){
-			System.out.println("works");
 			exits.remove(index);
 			return exits.get(0);
 		} else{
